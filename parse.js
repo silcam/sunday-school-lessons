428adf44ab70ccc05c5d0a9bdfdc3e5862847c9e
Rick Conrad <richardjconrad@gmail.com>
2018-11-14 13:47:28 +0100

    Update parser to grab only individual text nodes, and to keep the xpath for each node.

--- parse.js
+++ parse.js
@@ -8,11 +8,43 @@ const textNS = "urn:oasis:names:tc:opendocument:xmlns:text:1.0";
 // const xPath = `//xmlns:p[@xmlns:style-name='M.T._20_Text']`
 const xPath = "//xmlns:p[contains(@xmlns:style-name, 'M.T._20_Text')]";
 const nodes = xmlDoc.find(xPath, textNS);
-const strings = nodes
-  .map(node => node.text().trim())
-  .reduce((accumStrings, string) => {
+
+const translatableStrings = parseNodes(nodes);
+
+const justTheStrings = removeDuplicates(extractStrings(translatableStrings));
+
+function parseNode(node) {
+  if (node.type() == "text") {
+    // Node must have at least one word character
+    if (/\w/.test(node.text())) {
+      return [
+        {
+          xpath: node.path(),
+          text: node.text().trim()
+        }
+      ];
+    }
+    return [];
+  }
+
+  return parseNodes(node.childNodes());
+}
+
+function parseNodes(nodes) {
+  let translatableStrings = [];
+  for (let i = 0; i < nodes.length; ++i) {
+    translatableStrings = translatableStrings.concat(parseNode(nodes[i]));
+  }
+  return translatableStrings;
+}
+
+function extractStrings(translatableStrings) {
+  return translatableStrings.map(strObject => strObject.text);
+}
+
+function removeDuplicates(strings) {
+  return strings.reduce((accumStrings, string) => {
     if (!accumStrings.includes(string)) return accumStrings.concat([string]);
     return accumStrings;
   }, []);
-console.log(strings.length);
-console.log(strings);
+}
